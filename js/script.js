45cdfca9cd8ffdc500c554cf13b3320725e6620d
AVCoding <70842251+AVCoding@users.noreply.github.com>
2022-02-24 17:27:24 +0300

    miiiook

--- js/script.js
+++ js/script.js
@@ -143,7 +143,7 @@ peer.on("call", (call) => {
               
               setInterval(function(){
                 
-                if(remoteStream.getVideoTracks()[0].muted && document.querySelector("#video-"+ call.peer != null){
+                if(remoteStream.getVideoTracks()[0].muted && document.querySelector("#video-"+ call.peer) != null){
                   console.log(remoteStream.getVideoTracks()[0].muted);
    
                   document.querySelector("#video-"+ call.peer).remove();
