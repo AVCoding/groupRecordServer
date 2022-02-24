4c95c01e48f1dfc0c6fce3c9e32ca7e0826c2650
AVCoding <70842251+AVCoding@users.noreply.github.com>
2022-02-24 12:54:54 +0300

    dfsdfsdfsd

--- js/script.js
+++ js/script.js
@@ -73,7 +73,7 @@
       conn.on('close', function(){
         alert('close text');
         document.getElementById("videoCaller-" + peer.id).remove();
-        peer.close();
+        peer.destroy();
         // $("#videoCaller-" + peer.id + '"').remove();
       });
   }
